a13eb45794d11814f8de27d5ea3e52d9c776afb2
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-22 12:50:11 +0700

    task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -2,3 +2,18 @@
 // трёхзначными числами. Напишите программу, которая покажет количество 
 // чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+int[] array = new int[4];
+Random rand = new Random();
+int res =0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100,1000);
+    if (array[i]%2==0) // если элемент массива четный
+    {
+        res++;
+    }
+}
+Console.WriteLine(string.Join(" , ",array));
+Console.WriteLine(res);
